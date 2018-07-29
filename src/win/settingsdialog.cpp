fee8764185a917580e7687264148f1427358662d
Stypox <stypox@pm.me>
2018-07-29 15:17:29 +0200

    Started working on settings window

--- src/win/settingsdialog.cpp
+++ src/win/settingsdialog.cpp
@@ -1,14 +1,14 @@
 #include "settingsdialog.h"
 #include "ui_settingsdialog.h"
 
-SettingsDialog::SettingsDialog(QWidget *parent) :
-	QDialog(parent),
-	ui(new Ui::SettingsDialog)
-{
+SettingsDialog::SettingsDialog(Settings& settings, UserData& userData, QWidget *parent) :
+	QDialog{parent}, ui{new Ui::SettingsDialog},
+	m_settings{settings}, m_userData{userData},
+	m_settingsCopy{settings}, m_userDataCopy{userData} {
 	ui->setupUi(this);
-}
 
-SettingsDialog::~SettingsDialog()
-{
+	connect(ui->)
+}
+SettingsDialog::~SettingsDialog() {
 	delete ui;
 }
--- src/win/settingsdialog.h
+++ src/win/settingsdialog.h
@@ -2,6 +2,7 @@
 #define SETTINGSDIALOG_H
 
 #include <QDialog>
+#include "src/res/settings.h"
 
 namespace Ui {
 class SettingsDialog;
@@ -11,11 +12,28 @@ class SettingsDialog : public QDialog {
 	Q_OBJECT
 
 public:
-	explicit SettingsDialog(QWidget *parent = nullptr);
+	explicit SettingsDialog(Settings& settings, UserData& userData, QWidget *parent = nullptr);
 	~SettingsDialog();
 
 private:
+	void updateLabels();
+
+private slots:
+	void backup();
+	void resetSettings();
+
+	void ok();
+	void cancel();
+	void apply();
+
+private:
 	Ui::SettingsDialog *ui;
+
+	Settings& m_settings;
+	UserData& m_userData;
+
+	Settings m_settingsCopy;
+	UserData m_userDataCopy;
 };
 
 #endif // SETTINGSDIALOG_H
